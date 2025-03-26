af062f9bcd3afcdf5655a6b624c4d9f62ab4b375
MM <mouramathdev@gmail.com>
2025-03-26 08:58:49 -0300

    Initial commit

---
+++ src/main/java/dev/processamento/consumer/ProcessamentoConsumer.java
@@ -0,0 +1,16 @@
+package dev.processamento.consumer;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessamentoConsumer {
+
+    @RabbitListener(queues = "${broker.queue.processamento.name}")
+    public void listerProcessamentoQueue(@Payload String descricao){
+        System.out.println(descricao);
+    }
+
+}
